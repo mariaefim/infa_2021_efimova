bf0e4efa8415ec748baa6d697b0f70b959c40698
mariaefim <91070989+mariaefim@users.noreply.github.com>
2021-09-27 18:18:19 +0300

    Add files via upload

---
+++ злой смайл.py
@@ -0,0 +1,24 @@
+from graph import *
+penColor ("black")
+penSize (1)
+brushColor("yellow")
+circle (250,250,100)
+brushColor ("red")
+circle (210,230,20)
+brushColor("black")
+circle (210,230,9)
+brushColor("black")
+penSize (10)
+line (240,223,160,180)
+penSize (1)
+brushColor ("red")
+circle (290,230,16)
+brushColor("black")
+circle (290,230,7)
+penSize (10)
+line (264,220,344,192)
+penSize (20)
+line (210,300,290,300)
+
+
+
